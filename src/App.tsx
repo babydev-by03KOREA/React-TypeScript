a395f7bd31d821d71e3b1f81026ba8d871348822
박형주 <hyeongju1122@gmail.com>
2023-03-20 18:07:09 +0900

    AnimalComponents

--- src/App.tsx
+++ src/App.tsx
@@ -1,105 +1,131 @@
-import React, { useRef, useState } from 'react'
+import React, {useRef, useState} from "react";
 // import Card from "./typescript/components";
-import styled from 'styled-components'
-import { ChakraProvider } from '@chakra-ui/react'
-import { ThemeProvider } from '@emotion/react'
-import { extendedTheme, theme } from './style'
-import Join from './hooks/join/join'
-import UserList from './hooks/arrays/UserList'
-import CreateUser from './hooks/useRef/CreateUser'
-import RadioButton from './components/Radio'
+import styled from "styled-components";
+import {ChakraProvider} from "@chakra-ui/react";
+import {ThemeProvider} from "@emotion/react";
+import {extendedTheme, theme} from "./style";
+import Join from "./hooks/join/join";
+import UserList from "./hooks/arrays/UserList";
+import CreateUser from "./hooks/useRef/CreateUser";
+import RadioButton from "./components/Radio";
 import ShopList from "./personal/ShopList";
 import AnimalButton from "./components/AnimalButton";
+import {
+    animal01,
+    animal02,
+    animal03,
+    animal04,
+    animal05,
+    animal06,
+    animal07,
+    animal08,
+    animal09,
+    animal10,
+    animal11,
+    animal12
+} from "./yeonpick/Animal";
 
 const checkArray = [
-  { data: 0, text: '대학생' },
-  { data: 1, text: '대학원생' },
-  { data: 2, text: '취준생' },
-  { data: 3, text: '직장인' },
-]
+    {id: 0, text: "대학생"},
+    {id: 1, text: "대학원생"},
+    {id: 2, text: "취준생"},
+    {id: 3, text: "직장인"},
+];
 
 const selectArray = [
-  { data: 0, text: '유쌍' },
-  { data: 1, text: '무쌍' },
-]
+    {id: 0, text: "유쌍"},
+    {id: 1, text: "무쌍"},
+];
 
 const wantDating = [
-  { id: '1번', text: '1번', ml: -3 },
-  { id: '2번', text: '2번', ml: 62 },
-  { id: '3번', text: '3번', ml: 60 },
-  { id: '4번 이상', text: '4번 이상', ml: 48 },
-]
+    {id: "1번", text: "1번", ml: -3},
+    {id: "2번", text: "2번", ml: 62},
+    {id: "3번", text: "3번", ml: 60},
+    {id: "4번 이상", text: "4번 이상", ml: 48},
+];
+
+const animalArray = [
+    {id:1, text: "강아지", root: animal01},
+    {id:2, text: "고양이", root: animal02},
+    {id:3, text: "곰", root: animal03},
+    {id:4, text: "기린", root: animal04},
+    {id:5, text: "늑대", root: animal05},
+    {id:6, text: "부엉이", root: animal06},
+    {id:7, text: "사자", root: animal07},
+    {id:8, text: "여우", root: animal08},
+    {id:9, text: "원숭이", root: animal09},
+    {id:10, text: "토끼", root: animal10},
+    {id:11, text: "햄스터", root: animal11},
+    {id:12, text: "호랑이", root: animal12},
+];
 
 function App() {
-  const [inputs, setInputs] = useState({
-    username: '',
-    email: '',
-  })
+    const [inputs, setInputs] = useState({
+        username: "",
+        email: "",
+    });
 
-  const { username, email } = inputs
+    const {username, email} = inputs;
 
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target
-    setInputs({
-      ...inputs,
-      [name]: value,
-    })
-  }
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = e.target;
+        setInputs({
+            ...inputs,
+            [name]: value,
+        });
+    };
 
-  const [users, setUsers] = useState([
-    {
-      id: 1,
-      username: 'velopert',
-      email: 'public.velopert@gmail.com',
-    },
-    {
-      id: 2,
-      username: 'tester',
-      email: 'tester@example.com',
-    },
-    {
-      id: 3,
-      username: 'liz',
-      email: 'liz@example.com',
-    },
-  ])
+    const [users, setUsers] = useState([
+        {
+            id: 1,
+            username: "velopert",
+            email: "public.velopert@gmail.com",
+        },
+        {
+            id: 2,
+            username: "tester",
+            email: "tester@example.com",
+        },
+        {
+            id: 3,
+            username: "liz",
+            email: "liz@example.com",
+        },
+    ]);
 
-  const [job, setJob] = useState<number[]>([])
-  const [select, setSelected] = useState<number>(-1)
-  const nextId = useRef(4)
-  const onCreate = () => {
-    const user = {
-      id: nextId.current,
-      username,
-      email,
-    }
-    setUsers(users.concat(user))
+    const [job, setJob] = useState<number[]>([]);
+    const [select, setSelected] = useState<number>(-1);
+    const nextId = useRef(4);
+    const [animalCheck, animalChecked] = useState<number>(-1);
+    const onCreate = () => {
+        const user = {
+            id: nextId.current,
+            username,
+            email,
+        };
+        setUsers(users.concat(user));
 
-    setInputs({
-      username: '',
-      email: '',
-    })
-    nextId.current += 1
-  }
+        setInputs({
+            username: "",
+            email: "",
+        });
+        nextId.current += 1;
+    };
 
-  const [check, setCheck] = useState<string>(wantDating[0].id)
+    const [check, setCheck] = useState<string>(wantDating[0].id);
+    const [isCheck, setIsChecked] = useState<boolean>(false);
 
-  return (
-    <Container>
-      <MobileContainer>
-        <ThemeProvider theme={theme}>
-          <ChakraProvider theme={extendedTheme}>
-            {/*<RadioButton*/}
-            {/*  labelArr={wantDating}*/}
-            {/*  checked={check}*/}
-            {/*  onChange={setCheck}*/}
-            {/*/>*/}
-            <AnimalButton/>
-          </ChakraProvider>
-        </ThemeProvider>
-      </MobileContainer>
-    </Container>
-  )
+    return (
+        <Container>
+            <MobileContainer>
+                <ThemeProvider theme={theme}>
+                    <ChakraProvider theme={extendedTheme}>
+                        <AnimalButton animalArr={animalArray} item={animalCheck} setItem={animalChecked}/>
+                    </ChakraProvider>
+                </ThemeProvider>
+            </MobileContainer>
+        </Container>
+    );
 }
 
 const Container = styled.div`
@@ -110,7 +136,7 @@ const Container = styled.div`
   align-items: center;
   justify-content: center;
   font-family: Pretendard;
-`
+`;
 
 const MobileContainer = styled.div`
   width: 420px;
@@ -121,6 +147,6 @@ const MobileContainer = styled.div`
   @media screen and (max-width: 420px) {
     width: 100vw;
   }
-`
+`;
 
-export default App
+export default App;
--- src/components/AnimalButton.tsx
+++ src/components/AnimalButton.tsx
@@ -1,24 +1,110 @@
-import React from 'react';
+import React, {useState} from "react";
 import styled from "styled-components";
 import {motion} from "framer-motion";
-import {animal01} from '../yeonpick/Animal';
+import {CheckIcon} from "@chakra-ui/icons";
+import {theme} from "../style";
+
+type AnimalArray = {
+    id: number;
+    text: string;
+    root: string;
+}
 
 type Props = {
-    textArr: string;
+    animalArr: AnimalArray[];
+    item: number;
+    setItem: (value: number) => void;
 };
 
-const AnimalButton = () => {
+const AnimalButton = ({animalArr, item, setItem}: Props) => {
+
     return (
         <React.Fragment>
-            <Btn src={animal01}/>
+            <Box>
+                {
+                    animalArr.map((elem, index) => (
+                        <AnimalBox key={index}>
+                            <AnimalBtn src={elem.root} onClick={() => {setItem(elem.id)}} />
+                            {item === elem.id && <Cover>
+                                <div className="box"></div>
+                                <CheckIcon
+                                    className='icon'
+                                    boxSize={25}
+                                    color='chakraPrimary.500'
+                                />
+                            </Cover>}
+                        </AnimalBox>
+                    ))
+                }
+            </Box>
         </React.Fragment>
     );
 };
 
-const Btn = styled(motion.img)`
+const AnimalBox = styled.button`
+  position: relative;
+  overflow: hidden;
+
+  &:hover {
+    img {
+      filter: brightness(0.8);
+    }
+  }
+
+  &:active {
+    img {
+      filter: brightness(0.6);
+    }
+  }
+
+  img {
+    width: 72px;
+    height: 72px;
+    z-index: 1;
+    transition: filter 0.2s ease-out;
+  }
+`;
+
+const Box = styled.div`
+  width: 100%;
+  height: 360px;
+  padding-left: calc(50% - 360px / 2 + 48px);
+  padding-right: calc(50% - 360px / 2 + 48px);
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
+
+const AnimalBtn = styled(motion.img)`
   width: 72px;
   height: 72px;
   z-index: 1;
-`
+`;
+
+const Cover = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .box {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    opacity: 0.7;
+    background: white;
+    z-index: 2;
+  }
+
+  .icon {
+    opacity: 1;
+    z-index: 3;
+  }
+`;
 
 export default AnimalButton;
